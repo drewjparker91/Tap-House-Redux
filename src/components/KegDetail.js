842017a033186ccd54982365399d5fde6c4f9f62
drewparker <drewjparker91@gmail.com>
2020-11-20 11:12:28 -0800

    add functionality for deleting a ticket

--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -1,9 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Button from "react-bootstrap/button"
 
 function KegDetail(props){
 
-  const { keg } = props;
+  const { keg, onClickingDelete } = props;
 
   return (
     <React.Fragment>
@@ -13,13 +14,15 @@ function KegDetail(props){
       <h5>${keg.price}</h5>
       <h5>{keg.alcoholContent}% ABV</h5>
       <h5>{keg.pintsRemaining} Pints Remaining</h5>
+      <Button variant="danger" onClick={() => onClickingDelete(keg.id)}>Delete Keg</Button>
       <hr/>
     </React.Fragment>
   );
 }
 
 KegDetail.propTypes = {
-  keg: PropTypes.object
+  keg: PropTypes.object,
+  onClickingDelete: PropTypes.func
 }
 
 export default KegDetail;
