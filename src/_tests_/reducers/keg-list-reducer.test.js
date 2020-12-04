c265577e6a4f63cfc743e3dd5438eb34e2dd126e
drewparker <drewjparker91@gmail.com>
2020-12-04 09:41:45 -0800

    add action ADD_KEG into keg-list-reducer.js and pass first two tests in keg-list-reducer.test.js

--- src/_tests_/reducers/keg-list-reducer.test.js
+++ src/_tests_/reducers/keg-list-reducer.test.js
@@ -1,7 +1,42 @@
 import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
+
+  let action;
+  const kegData ={
+    name: 'Corona',
+    brand: 'Corona Co',
+    price: '5.99',
+    alcoholContent: '5.0',
+    pintsRemaining: '18',
+    id: 1
+  }
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
+  });
+
+  test('Should successfully add new keg data to masterKegList', () => {
+    const { name, brand, price, alcoholContent, pintsRemaining, id } = kegData;
+    action = {
+      type: 'ADD_KEG',
+      name: name,
+      brand: brand,
+      price: price,
+      alcoholContent: alcoholContent,
+      pintsRemaining: pintsRemaining,
+      id: id
+    };
+
+    expect(kegListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        brand: brand,
+        price: price,
+        alcoholContent: alcoholContent,
+        pintsRemaining: pintsRemaining,
+        id: id
+      }
+    })
   });
 });
