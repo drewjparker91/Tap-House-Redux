e559746f68af78fac6caf9d7fd1fb63a0e2e82b7
drewparker <drewjparker91@gmail.com>
2020-12-04 10:59:52 -0800

    pass test for checking ADD_Keg action functioning for both kegListReducer and rootReducer

---
+++ src/_tests_/reducers/index-reducer.test.js
@@ -0,0 +1,39 @@
+import rootReducer from '../../reducers/index';
+import { createStore } from 'redux';
+import formVisibleReducer from '../../reducers/form-visible-reducer';
+import kegListReducer from '../../reducers/keg-list-reducer';
+
+let store = createStore(rootReducer);
+
+describe("rootReducer", () => {
+
+  test('should return default state if no action type is recognized', () => {
+    expect(rootReducer({}, {type: null })).toEqual({
+      masterKegList: {},
+      formVisibleOnPage: false
+    });
+  });
+
+  test('Check that initial state of kegListReducer matches root reducer', () => {
+    expect(store.getState().masterKegList).toEqual(kegListReducer(undefined, { type: null }));
+  });
+
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
+  });
+
+  test('Check that ADD_TICKET action works for ticketListReducer and root reducer', () => {
+    const action = {
+      type: 'ADD_KEG',
+      name: 'Corona',
+      brand: 'The Corona Co.',
+      price: '5.99',
+      alcoholContent: '5.1',
+      pintsRemaining: '14',
+      id: 1
+    }
+    store.dispatch(action);
+    expect(store.getState().masterKegList).toEqual(kegListReducer(undefined, action));
+  });
+
+});
