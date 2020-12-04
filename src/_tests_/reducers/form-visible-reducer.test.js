69266ae08d2127fed150090b17045f6a83a3e8a3
drewparker <drewjparker91@gmail.com>
2020-12-04 10:45:19 -0800

    add switch case to form-visible-reducer.js and pass second test in form-visible-reducer.test.js for toggling form visibility state to true

--- src/_tests_/reducers/form-visible-reducer.test.js
+++ src/_tests_/reducers/form-visible-reducer.test.js
@@ -5,4 +5,8 @@ describe('formVisibleReducer', () => {
   test('should return default state if no action type is recognized', () => {
     expect(formVisibleReducer(false, {type: null})).toEqual(false);
   });
+
+  test('should toggle form visibility state to true', () => {
+    expect(formVisibleReducer(false, {type: 'TOGGLE_FORM'})).toEqual(true);
+  });
 });
