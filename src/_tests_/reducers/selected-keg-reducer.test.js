7cd169e1d07fa829dd4f24dbf9b58af18a369564
drewparker <drewjparker91@gmail.com>
2020-12-04 11:25:54 -0800

    pass test for toggling to selected keg in selected-keg-reducer.test.js

--- src/_tests_/reducers/selected-keg-reducer.test.js
+++ src/_tests_/reducers/selected-keg-reducer.test.js
@@ -15,4 +15,19 @@ describe("selectedKegReducer", () => {
   test('Should return default state if no action type is recognized', () => {
     expect(selectedKegReducer(null, {type: null})).toEqual(null);
   });
-})
+
+  test('Should toggle to selected keg', () => {
+    action = {
+      type: 'SELECTED_KEG',
+      selectedKeg: selectedKeg
+    };
+    expect(selectedKegReducer(selectedKeg, action)).toEqual({
+      name: 'Corona',
+      brand: 'The Corona Co',
+      price: '5.99',
+      alcoholContent: '5.1',
+      pintsRemaining: '18',
+      id: 1
+    });
+  });
+});
